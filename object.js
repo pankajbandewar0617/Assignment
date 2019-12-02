04698bd274b98a6dfb20fd8394fd41f101346104
Pankaj Bandewar <pankaj.bandewar76@gmail.com>
2019-12-02 17:36:17 +0530

    Object Assignment

---
+++ object.js
@@ -0,0 +1,65 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; // use this object to test your functions
+const _ = require("underscore")
+
+// Complete the following underscore functions.
+// Reference http://underscorejs.org/ for examples.
+
+function keys(obj) {
+  // Retrieve all the names of the object's properties.
+  // Return the keys as strings in an array.
+  // Based on http://underscorejs.org/#keys
+let result =  _.keys(obj);
+console.log(result)
+}
+keys(testObject);
+
+
+function values(obj) {
+  // Return all of the values of the object's own properties.
+  // Ignore functions
+  // http://underscorejs.org/#values
+  let result = _.values(obj)
+  console.log(result)
+}
+values(testObject);
+
+
+function mapObject(obj, cb) {
+  // Like map for arrays, but for objects. Transform the value of each property in turn by passing it to the callback function.
+  // http://underscorejs.org/#mapObject
+  let result = _.mapObject(obj,function(val){
+    return val + cb
+  })
+  console.log(result)
+}
+mapObject(testObject,' ok')
+
+
+function pairs(obj) {
+  // Convert an object into a list of [key, value] pairs.
+  // http://underscorejs.org/#pairs
+  let result = _.pairs(obj);
+  console.log(result)	
+};
+pairs(testObject);
+
+/* STRETCH PROBLEMS */
+
+function invert(obj) {
+  // Returns a copy of the object where the keys have become the values and the values the keys.
+  // Assume that all of the object's values will be unique and string serializable.
+  // http://underscorejs.org/#invert
+    let result = _.invert(obj);
+    console.log(result)
+};
+invert(testObject);
+
+function defaults(obj, defaultProps) {
+  // Fill in undefined properties that match properties on the `defaultProps` parameter object.
+  // Return `obj`.
+  // http://underscorejs.org/#defaults
+  defaultProps = {name:'Superman', symbol: 'bat'}
+  let result = _.defaults(obj,defaultProps)
+  console.log(result)
+};
+defaults(testObject)
